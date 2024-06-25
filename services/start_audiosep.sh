4233f6bf2c35321aac23ff7881bbda60599dd55e
Utkarsh Raj <raj.utkarsh001@gmail.com>
2024-06-25 09:15:03 +0530

    starlette

--- services/start_audiosep.sh
+++ services/start_audiosep.sh
@@ -1,5 +1,5 @@
 export AUDIOSEP_SERVICE_PORT=$((${SERVICE_PORT}+2))
 
-conda activate AudioEditor
+conda activate WavCraft
 nohup python3 services/audiosep_service.py > services_logs/audiosep.out 2>&1 &
 echo "AudioSep is loaded sucessfully."
