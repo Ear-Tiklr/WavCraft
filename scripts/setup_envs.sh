48cab9cce61a08363823ca28532d05cb10d7c566
Utkarsh Raj <raj.utkarsh001@gmail.com>
2024-06-25 07:54:45 +0530

    fixes

--- scripts/setup_envs.sh
+++ scripts/setup_envs.sh
@@ -1,4 +1,27 @@
-conda env create -f venvs/audiocraft.yml
-conda env create -f venvs/audioldm.yml
-conda env create -f venvs/audiosr.yml
-conda env create -f venvs/wavcraft.yml
+#!/bin/bash
+
+# Function to create and activate Conda environment
+create_and_activate() {
+    env_name=$1
+    file_path=$2
+
+    echo "Creating Conda environment: $env_name"
+    conda env create -f $file_path
+    if [ $? -eq 0 ]; then
+        echo "$env_name environment created successfully."
+    else
+        echo "Failed to create $env_name environment. Attempting to activate if it exists..."
+        conda activate $env_name
+        if [ $? -eq 0 ]; then
+            echo "Activated existing $env_name environment."
+        else
+            echo "Failed to activate $env_name environment. It may not exist."
+        fi
+    fi
+}
+
+# Paths to the YAML files for environment creation
+create_and_activate "AudioCraft" "venvs/audiocraft.yml"
+create_and_activate "AudioLDM" "venvs/audioldm.yml"
+create_and_activate "AudioSR" "venvs/audiosr.yml"
+create_and_activate "WavCraft" "venvs/wavcraft.yml"
